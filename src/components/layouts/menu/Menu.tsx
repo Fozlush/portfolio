556ecbe8cc037d76541f6cf052f3f7838b46c3f5
Fozlush <Fozlush@gmail.com>
2023-05-15 02:55:38 +0300

    страница обо мне

--- src/components/layouts/menu/Menu.tsx
+++ src/components/layouts/menu/Menu.tsx
@@ -6,10 +6,6 @@ import style from './menu.module.scss'
 const Menu: FC = () => {
   return (
     <menu className={style.menu}>
-      <div className={style.top}>
-        <span className={style.logo}>Я</span>
-        <span className={style.logo}>F</span>
-      </div>
       <nav className={style.navigation}>
         <ul>
           {links.map((data) => {
@@ -17,7 +13,6 @@ const Menu: FC = () => {
           })}
         </ul>
       </nav>
-      <div className={style.social}></div>
     </menu>
   )
 }
--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,9 +1,15 @@
 import { FC } from "react"
+import style from './about.module.scss'
 
 const AboutPage: FC = () => {
   return (
     <div>
-      About
+      <p className={style.greetings}>Привет, давай знакомиться, я -</p>
+      <p className={style.name}><span>Я</span>рослав <span>К</span>лещев</p>
+      <p className={style.profession}><span>frontend</span> разработчик</p>
+      <p className={style.description}>Свой первый hello world написал в апреле 2021. К сегодняшнему дню закончил курс frontend разработчика от NordicIT, написал с десяток самостоятельных проектов, получил опыт коммерческой разработки.</p>
+      <p className={style.experience}>июль 2022 - март 2023 frontend разработчик в команде билинга AMOCRM</p>
+      <p className={style.technology}>Мой стэк технологий: HTML, SCSS, JS, jquery, React, Redux, Underscore, Backbone, npm/yarn, Git</p>
     </div>
   )
 }
