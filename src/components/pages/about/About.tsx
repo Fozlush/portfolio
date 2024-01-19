ec507edf8cff7d487cd203a6dca41a8dd21f541d
Fozlush <Fozlush@gmail.com>
2024-01-19 15:03:51 +0300

    fix

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -8,7 +8,7 @@ const AboutPage: FC = () => {
       <p className={style.name}><span>Я</span>рослав <span>К</span>лещев</p>
       <p className={style.profession}><span>frontend</span> разработчик</p>
       <p className={style.description}>Свой первый hello world написал в апреле 2021. К сегодняшнему дню закончил курс frontend разработчика от NordicIT, написал с десяток самостоятельных проектов, получил опыт коммерческой разработки.</p>
-      <p className={style.experience}>июль 2022 - сентябрь 2023 frontend разработчик в команде билинга AMOCRM</p>
+      <p className={style.experience}>июль 2022 - март 2023 frontend разработчик в команде билинга AMOCRM</p>
       <p className={style.technology}>Мой стэк технологий: HTML, SCSS, JS, jquery, React, Redux, Underscore, Backbone, npm/yarn, Git</p>
     </div>
   )
