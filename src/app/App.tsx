356ed7cf7af7af79560f6b3d0ede7cd235dd5c1f
Fozlush <Fozlush@gmail.com>
2025-08-12 01:42:05 +0300

    greetings and fonts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import { Contacts } from "../components/pageBlocks"
+import { Greetings } from "../components/pageBlocks/greetings/Greetings"
 import { ThemeToggle } from "../components/themeToggle/ThemeToggle"
 import "./styles/global.scss"
 
 function App() {
   return (
     <>
+      <Greetings />
       <Contacts />
-      <ThemeToggle/>
+      <ThemeToggle />
     </>
   )
 }
